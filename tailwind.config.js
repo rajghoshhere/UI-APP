4f0a0622f7fe5ac08aa907ab610b12a9f20c21df
rajghoshhere <rajesh.ghosh88@gmail.com>
2024-12-27 18:13:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.ts
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',  // Includes your custom TypeScript/JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
